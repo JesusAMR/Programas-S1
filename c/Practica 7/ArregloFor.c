83b1c93f15f4f8f6e7919738ab63610d59e1c0de
JesusMR <jesusmiranda_6@hotmail.com>
2016-03-18 14:38:32 -0600

    commit

--- c/Practica 7/ArregloFor.c
+++ c/Practica 7/ArregloFor.c
@@ -1,15 +1,22 @@
 #include<stdio.h>
 
 int main(){
-    int n,i;
-    printf("Inserte el numero de elementos entero del arreglo");
-   // scanf("%d",&n);
-    int a[10];
-    for (i=0;i<10;i++){
-        printf("Inserte el valor del arreglo en posicion [%d] \n",i);
-        scanf("%d \n",&a[i]);
+    int n,i,pm=0,sm=0;
+    printf("Inserte el numero de elementos entero del arreglo\n");
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++){
+        printf("Inserte el num. %d:\n",i+1);
+        scanf("%d",&a[i]);
+        pm+=a[i];
     }
-    for(i=0;i<10;i++)
-        printf("%d\n",a[i]);
+    pm=pm/n;
+    printf("Listado del arreglo: \n");
+    for(i=0;i<n;i++){
+        printf("%d \n",a[i]);
+        if(a[i]>pm)
+            sm+=a[i];
+    }
+    printf("Promedio: %d, suma de numeros mayores al promedio : %d",pm,sm);
     return 0;
 }
